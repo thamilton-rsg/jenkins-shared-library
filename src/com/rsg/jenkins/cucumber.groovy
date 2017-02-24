ebcdd8ac1f0afff15bdc9785d544e6849c4169f5
thamilton-rsg <thamilton@rsglab.com>
2017-02-23 22:29:20 -0500

    removed semi-colon

--- src/com/rsg/jenkins/cucumber.groovy
+++ src/com/rsg/jenkins/cucumber.groovy
@@ -1,12 +1,11 @@
 package com.rsg.jenkins
 
-
 class Cucumber implements Serializable {
 
     private org.jenkinsci.plugins.workflow.cps.CpsScript script
 
     public Cucumber(org.jenkinsci.plugins.workflow.cps.CpsScript script) {
-        this.script = script;
+        this.script = script
     }
 
     public <V> V withOptions(String test) {
