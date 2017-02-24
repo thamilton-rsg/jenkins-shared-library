0d3c50d66845213cd0a57f79e2834b119540e921
thamilton-rsg <thamilton@RSG-5380-Thomas-Hamilton-MBPro.local>
2017-02-23 21:49:05 -0500

    Initial commit

---
+++ src/com/rsg/jenkins/cucumber.groovy
@@ -0,0 +1,18 @@
+package com.rsg.jenkins
+
+
+class Cucumber implements Serializable {
+
+    private org.jenkinsci.plugins.workflow.cps.CpsScript script
+
+    public Cucumber(org.jenkinsci.plugins.workflow.cps.CpsScript script) {
+        this.script = script;
+    }
+
+    public <V> V withOptions(String test) {
+        node {
+            script.sh "ls -l"
+        }
+    }
+
+}
