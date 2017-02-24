266083c1e68b737658ce1184d3ae278239521a88
thamilton-rsg <thamilton@rsglab.com>
2017-02-23 22:37:01 -0500

    removed public

--- src/com/rsg/jenkins/cucumber.groovy
+++ src/com/rsg/jenkins/cucumber.groovy
@@ -4,11 +4,11 @@ class Cucumber implements Serializable {
 
     private org.jenkinsci.plugins.workflow.cps.CpsScript script
 
-    public Cucumber(org.jenkinsci.plugins.workflow.cps.CpsScript script) {
+    Cucumber(org.jenkinsci.plugins.workflow.cps.CpsScript script) {
         this.script = script
     }
 
-    public <V> V withOptions(String test) {
+    def <V> V withOptions(String test) {
         node {
             script.sh "ls -l"
         }
